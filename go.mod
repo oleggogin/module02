c39a7e7b8554fdccbf5bc6000803fc8e6ed074f9
oleggogin <you@example.com>
2022-12-12 14:36:10 +0300

    one

---
+++ go.mod
@@ -0,0 +1,3 @@
+module rebrain
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	run := "quickly"
+	name := "Gosha"
+	age := 33
+	sex := "male"
+	fmt.Println(run, name, age, sex)
+}
